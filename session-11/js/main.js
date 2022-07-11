7cd48d74613af1c470ecded3b9236a50b97e5c05
Norman <80184728+norgru@users.noreply.github.com>
2022-07-11 10:16:35 +0100

    yes

---
+++ session-11/js/main.js
@@ -0,0 +1,46 @@
+/*
+    CalculatorButton
+        operator
+
+    CalculatorButtns[]
+
+    var calculatorButtonsContainerElement = document.querySelector("#calculator.buttons");
+
+    foreach object in calculatorButtonsArray
+        create a button child in calculatorButtonsContainerElement
+        add object.operator to button child
+        button onclick = calculatorButtonOnClick(object)
+
+*/
+
+
+var calculatorButtonsArray = [
+    {
+        operator: "1",
+        onClick: (currentValue, operator) => {
+            return currentValue.concat(operator);
+        }
+    },   
+    {
+        operator: "=",
+        onClick: (currentValue) => {
+            //calculate string. todo
+            return currentValue + " wanker wanker wanker wanker";
+        }
+    } 
+]
+
+var display = "";
+
+const calculatorButtonOnClick = (button) => {
+    console.log(button.operator, "has been clicked");
+    var newValueFromOnClick = button.onClick(display, operator);
+    display = newValueFromOnClick;  
+}
+
+
+document.addEventListener("DOMContentLoaded", function() {
+
+    console.log("hello");
+    // code...
+});

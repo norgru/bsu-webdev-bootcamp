a8a9387de57be98341391225e751f54aef016811
Norman <80184728+norgru@users.noreply.github.com>
2022-07-20 10:28:18 +0100

    dfgh

---
+++ session-17/main.js
@@ -0,0 +1,37 @@
+let music = [
+    {
+        name: 'naive response',
+        genre: 'techno',
+        rating: '9',
+        esoteric: '7'
+    },
+
+    {
+        name: 'agua de beber',
+        genre: 'bossa nova',
+        rating: '9',
+        esoteric: '4'
+    },
+
+    {
+        name: 'naive response',
+        genre: 'techno',
+        rating: '9',
+        esoteric: '7'
+    }
+]
+
+let numbers = [
+    15,
+    78,
+    3,
+    9,
+    32
+]
+
+let notes = [
+    'C4',
+    'G#3',
+    'E2',
+    'A#5'
+]

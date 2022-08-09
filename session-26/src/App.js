92e77111b8980a6c45b2969e167810a17a35872e
Norman <80184728+norgru@users.noreply.github.com>
2022-08-09 12:32:22 +0100

    session-26-time

--- session-26/src/App.js
+++ session-26/src/App.js
@@ -1,25 +1,94 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from "react";
 
 function App() {
+
+  const peopleList = [
+    {
+      personName: 'John',
+      email: 'john@johnmail.com'
+    },
+    {
+      personName: 'Sarah',
+      email: 'sarah@sarahmail.com'
+    },
+    {
+      personName: 'Ed',
+      email: 'ed@edmail.com'
+    }
+  ]
+
+  const [people, setPeople] = useState(peopleList);
+
+  const [name, setName] = useState('');
+
+  const [email, setEmail] = useState('');
+
+  function addPerson(){
+    console.log('add person function');
+    setPeople([
+      ...people,
+      {
+        'personName': name,
+        'email': email
+      }
+    ])
+    
+  }
+
+  function handleChange(){
+    console.log('handleChange');
+  }
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    console.log('submitted');
+    addPerson();
+  }
+
+  
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+
+      <form onSubmit={submitHandler}>
+          <label>
+              Name:
+              <input type="text" value={name} onChange={(event) => setName(event.target.value)} />
+          </label>
+          <label>
+              email:
+              <input type="text" value={email} onChange={(event) => setEmail(event.target.value)} />
+          </label>
+          <input type="submit" value="Submit" />
+      </form>
+
+      {
+        people.map(function(person, index){
+          return(
+            <div>
+              {person.personName}
+              <hr></hr>
+              {person.email}
+            </div>
+
+          )
+
+        })
+
+      }
+
     </div>
   );
 }
 
+
+
+//const handleC
+
 export default App;
+
+//Create form
+//Add l

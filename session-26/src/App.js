15ead88856019478a9f972b578d149358e76225f
Norman <80184728+norgru@users.noreply.github.com>
2022-08-10 13:53:17 +0100

    gfs

--- session-26/src/App.js
+++ session-26/src/App.js
@@ -1,21 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 function App() {
 
   const peopleList = [
     {
       personName: 'John',
-      email: 'john@johnmail.com'
+      email: 'john@johnmail.com',
+      meals: []
     },
     {
       personName: 'Sarah',
-      email: 'sarah@sarahmail.com'
+      email: 'sarah@sarahmail.com',
+      meals: []
     },
     {
       personName: 'Ed',
-      email: 'ed@edmail.com'
+      email: 'ed@edmail.com',
+      meals: []
     }
   ]
 
@@ -25,21 +28,43 @@ function App() {
 
   const [email, setEmail] = useState('');
 
+  const meals = ["Breakfast", "Lunch"];
+
+  const [checked, setChecked] = useState([]);
+
+  const handleCheck = (event) => {
+    let updatedList = [...checked];
+    if (event.target.checked) {
+        updatedList = [...checked, event.target.value];
+    } else {
+        updatedList.splice(checked.indexOf(event.target.value), 1);
+    }
+    setChecked(updatedList);
+  };
+
   function addPerson(){
     console.log('add person function');
     setPeople([
       ...people,
       {
         'personName': name,
-        'email': email
+        'email': email,
+        'meals': checked
       }
     ])
     
   }
 
-  function handleChange(){
-    console.log('handleChange');
-  }
+  useEffect(() => {
+    console.log('Component mounted');
+}, [])
+
+
+
+useEffect(() => {
+    console.log(checked);
+}, [checked])
+
 
   const submitHandler = (e) => {
     e.preventDefault();
@@ -53,8 +78,14 @@ function App() {
         addPerson();
         console.log('submitted');
       }
+      else if(regex.test(email)){
+        console.log('hi');
+
+      }
       else{
+        
         console.log('false name');
+        window.alert('NO!')
       }
     }
     validateInputs();
@@ -66,6 +97,9 @@ function App() {
   return (
     <div className="App">
       
+      <div className='loginText'>
+        <h1>Log in</h1>
+      </div>
 
       <form onSubmit={submitHandler}>
           <label>
@@ -73,10 +107,17 @@ function App() {
               <input type="text" value={name} onChange={(event) => setName(event.target.value)} />
           </label>
           <label>
-              email:
+              E-mail:
               <input type="text" value={email} onChange={(event) => setEmail(event.target.value)} />
           </label>
-          <input type="checkbox" value="tick" />
+          <div>
+              {meals.map((item, index) => (
+                  <div key={index}>
+                      <input value={item} type="checkbox" onChange={handleCheck}/>
+                      <span>{item}</span>
+                  </div>
+              ))}
+          </div>
           <input type="submit" value="Submit" />
       </form>
 
@@ -87,6 +128,8 @@ function App() {
               {person.personName}
               <br></br>
               {person.email}
+              <br></br>
+              {person.meals}
               <hr></hr>
             </div>
 
---
+++ session-27/src/App.js
@@ -0,0 +1,66 @@
+import './App.css';
+
+import {useState, useEffect} from 'react';
+
+import RadioButtons from './components/RadioButtons'
+
+function App() {
+
+  //First we make the meal options
+  const mealOptions = ['Fish & chips', 'Lobster Bisque', 'Mushroom soup']
+  
+  //Initiate the state variable for the selected meal options
+  const [selectedMeals, setSelectedMeals] = useState([]);
+
+  const handleCheck = (e) => {
+    //This is the function that fires when we click on a checkbox
+    //The aim is to update the selected meals array
+    //First we need to createa a duplicate array and add the items that have been checked to it, then we update the selectedMeals state afterwards
+    
+    //console.log(e.target.checked);
+
+    let tempArray = [...selectedMeals];
+
+    if(e.target.checked){
+      console.log('TRUE - ITEM IS CHECKED, ADD IT TO THE ARRAY');
+      //use assignment operator to assign new value to the tempArray
+      tempArray = [...selectedMeals, e.target.value]
+      console.log(tempArray)
+    }
+    else{
+      //if the item is not checked remove it from the tempArray
+      tempArray.splice(selectedMeals.indexOf(e.target.value), 1)
+      console.log(tempArray)
+    }
+    setSelectedMeals(tempArray);
+    
+  }
+
+  //Check the selectedMeals after it has been changed
+  //useEffect can be called
+
+  useEffect(() => {
+    console.log(selectedMeals);
+  }, [selectedMeals])
+  
+
+  return (
+    <div className="App">
+      
+      <form>
+              {/*Iterate through the mealOptions array and display a checkbox for each one */}
+              {mealOptions.map((item, index) => (
+                  <div key={index}>
+                      <input value={item} type="checkbox" onChange={handleCheck}/>
+                      <span>{item}</span>
+                  </div>
+              ))}
+      </form>
+
+      <RadioButtons></RadioButtons>
+
+    </div>
+  );
+}
+
+export default App;
